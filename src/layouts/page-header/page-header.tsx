ebbf955e319477fda73872ac1ee512a4a1e54f28
Burton Smith <burton@breakstuff.io>
2021-01-21 20:50:25 -0500

    page header and footer styles

--- src/layouts/page-header/page-header.tsx
+++ src/layouts/page-header/page-header.tsx
@@ -1,6 +1,7 @@
-import { Component, h, State, ComponentInterface, Host } from '@stencil/core';
+import { Component, h, State, ComponentInterface, Host, Element } from '@stencil/core';
 import { getAllCategories, Category } from '../../utils/categoryUtils';
 import { sortArray } from "../../utils/arrayUtils";
+import state from "../../store/cartStore";
 
 @Component({
     tag: 'page-header',
@@ -8,6 +9,10 @@ import { sortArray } from "../../utils/arrayUtils";
 })
 export class PageHeader implements ComponentInterface {
     $mobileMenu: HTMLKsSideDrawerElement;
+    $cartSummary: HTMLCartSummaryElement;
+
+    @Element() $el: HTMLElement;
+
     @State() categories: Category[];
 
     async componentWillLoad() {
@@ -18,6 +23,8 @@ export class PageHeader implements ComponentInterface {
     componentDidRender() {
         Array.from(this.$mobileMenu.querySelectorAll('a'))
             .forEach(element => element.addEventListener('click', () => this.$mobileMenu.hide()));
+        
+        this.$cartSummary = this.$el.querySelector('cart-summary');
     }
 
     render() {
@@ -47,9 +54,9 @@ export class PageHeader implements ComponentInterface {
                                         <ks-icon icon="search" label="search"></ks-icon>
                                     </ks-button>
                                 </ks-form>
-                                <ks-button display="link" class="cart-link mx-sm position-relative">
+                                <ks-button display="link" class="cart-link mx-sm position-relative" onClick={() => this.$cartSummary.show()}>
                                     <ks-icon icon="cart" class="text-md"></ks-icon>
-                                    <ks-badge color="primary" class="bg-primary position-absolute right-0 top-0 offset-x -offset-y">3</ks-badge>
+                                    <ks-badge color="primary" class={`bg-primary position-absolute right-0 top-0 offset-x -offset-y ${state.count > 0 ? '' : 'sr-only'}`}>{state.count}</ks-badge>
                                 </ks-button>
                             </div>
                         </div>
@@ -59,9 +66,9 @@ export class PageHeader implements ComponentInterface {
                             <ks-icon icon="menu" label="menu"></ks-icon>
                         </ks-button>
                         <stencil-route-link class="main-link" url="/">Chronos</stencil-route-link>
-                        <ks-button display="link" class="cart-link mx-sm position-relative">
+                        <ks-button display="link" class="cart-link mx-sm position-relative" onClick={() => this.$cartSummary.show()}>
                             <ks-icon icon="cart" class="text-md"></ks-icon>
-                            <ks-badge color="primary" class="bg-primary position-absolute right-0 top-0 offset-x -offset-y">3</ks-badge>
+                            <ks-badge color="primary" class={`bg-primary position-absolute right-0 top-0 offset-x -offset-y ${state.count > 0 ? '' : 'sr-only'}`}>{state.count}</ks-badge>
                         </ks-button>
                     </div>
                     <ks-side-drawer id="mobile_menu" header-text="Chronos" position="left" class="mobile-links" ref={el => this.$mobileMenu = el}>
@@ -75,6 +82,7 @@ export class PageHeader implements ComponentInterface {
                         <stencil-route-link url="/about">About</stencil-route-link>
                         <stencil-route-link url="/contact">Contact</stencil-route-link>
                     </ks-side-drawer>
+                    <cart-summary></cart-summary>
                 </header>
             </Host>
         );
