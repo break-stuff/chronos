10fdc91036c01c3e8917afd20c4e34406d19d35c
Burton Smith <burton@breakstuff.io>
2021-01-28 22:16:54 -0500

    update page links

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -12,7 +12,7 @@ export class HomePage {
                     <ks-card-body class="h-100 display-flex flex-column justify-center align-end text-center">
                         <div class="text-lg xs:text-sm">A demo e-commerce site built <span class="mr-xs text-italic text-underline">entirely</span> with</div>
                         <div class="text-xxl xs:text-lg text-uppercase">Web Components</div>
-                        <ks-button size="lg"><ks-icon icon="github" class="mr-sm"></ks-icon> View Source Code</ks-button>
+                        <ks-button size="lg" href="https://github.com/break-stuff/chronos" target="_blank"><ks-icon icon="github" class="mr-sm"></ks-icon> View Source Code</ks-button>
                     </ks-card-body>
                 </ks-card>
                 <ks-card img-src="/assets/images/watch_on_carpet.jpg" alt="watch on carpet" img-direction="left" collapse="sm" img-aspect-ratio="4:3" class="promo">
@@ -21,7 +21,7 @@ export class HomePage {
                         <p class="mb-lg">
                             The bulk of the site is made entirely of web components. The pages and layouts are compiled using <a href="https://stenciljs.com/" target="_blank" rel="noopener">Stencil.js</a> and <a href="https://kickstand-ui.com/" target="_blank" rel="noopener">Kickstand UI</a> is used for the Design System/Component library.
                         </p>
-                        <ks-button display="hollow">View Source Code</ks-button>
+                        <ks-button display="hollow" href="https://github.com/break-stuff/chronos" target="_blank">View Source Code</ks-button>
                     </ks-card-body>
                 </ks-card>
                 <div class="my-md">&nbsp;</div>
