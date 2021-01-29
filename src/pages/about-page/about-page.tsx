ce44c4e525e0f44576da073a05883e4651e7cd8c
Burton Smith <burton@breakstuff.io>
2021-01-28 22:17:38 -0500

    add content

--- src/pages/about-page/about-page.tsx
+++ src/pages/about-page/about-page.tsx
@@ -14,6 +14,37 @@ export class AboutPage {
             <div class="mx-auto w-50 md:w-75 sm:w-100 my-xl p-xxxl b-xxxs bg-white">
                 <h1>About This Site</h1>
 
+                <p>This site is built without any JavaScript frameworks! I know what some of you are thinking - <em>"Why would you do that to yourself???"</em></p>
+
+                <p>Well, for 2 reasons:</p>
+
+                <ol>
+                    <li>It's <em>wicked</em> fast!</li>
+                    <li>It wasn't that hard (thanks to some awesome tools).</li>
+                </ol>
+
+                <h2>Performance</h2>
+
+                <p>
+                    Thanks to the tools mentioned below, this site compiles down to standards-based web components. 
+                    Web components have been around for a while, but their browser support hasn't been that great. 
+                    That is no longer the case! Support is pretty decent and what isn't supported is pretty easily polyfilled.
+                    Now, you can bundle up your components and render them using the browser rather than a JavaScript library (which, again, makes them incredibly fast).
+                </p>
+
+                <h2>Tools</h2>
+
+                <p>There were a few tools and libraries used to make building this site much easier:</p>
+
+                <ul>
+                    <li><ks-button target="_blank" display="link" class="text-bold" href="https://stenciljs.com/">Stencil</ks-button> - For compiling the pages, layouts, and component into web components</li>
+                    <li><ks-button target="_blank" display="link" class="text-bold" href="https://github.com/ionic-team/stencil-router/wiki">Stencil Router</ks-button> - For providing the Single-Page Application (SPA) routing</li>
+                    <li><ks-button target="_blank" display="link" class="text-bold" href="https://stenciljs.com/docs/stencil-store#stencil-store">Stencil Store</ks-button> - For state management</li>
+                    <li><ks-button target="_blank" display="link" class="text-bold" href="https://kickstand-ui.com/">Kickstand UI</ks-button> - The design system and UI library to build the site</li>
+                </ul>
+
+                <p>If you haven't had a chance to play with any of these tools, I highly recommend them.</p>
+
                 <h2>Media</h2>
 
                 <p>We are grateful for the following talented artists at <ks-button href="https://unsplash.com/" display="link" target="_blank">Unsplash</ks-button> for providing the stock photos:</p>
